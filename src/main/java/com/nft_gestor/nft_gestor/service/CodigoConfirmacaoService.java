e3dcdb75025bd584899405d70870ebb1d7816bb5
Fabrício Ferreira Tavares <fabriciodev959@gmail.com>
2026-08-11 00:38:08 -0300

    Alteração no envio de emails

--- src/main/java/com/nft_gestor/nft_gestor/service/CodigoConfirmacaoService.java
+++ src/main/java/com/nft_gestor/nft_gestor/service/CodigoConfirmacaoService.java
@@ -2,12 +2,13 @@ package com.nft_gestor.nft_gestor.service;
 
 import com.nft_gestor.nft_gestor.exception.RequestException;
 import com.nft_gestor.nft_gestor.model.CodigoConfirmacaoModel;
-import com.nft_gestor.nft_gestor.model.UsuarioModel;
 import com.nft_gestor.nft_gestor.repository.CodigoConfirmacaoRepository;
 import com.nft_gestor.nft_gestor.repository.UsuarioRepository;
+import com.resend.Resend;
+import com.resend.core.exception.ResendException;
+import com.resend.services.emails.model.CreateEmailOptions;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -22,69 +23,76 @@ public class CodigoConfirmacaoService {
     @Autowired
     private UsuarioRepository usuarioRepository;
 
+    private final Resend resend;
+
     @Autowired
-    private JavaMailSender mailSender;
+    public CodigoConfirmacaoService(
+            @Value("${resend.api-key}") String apiKey
+    ) {
+        this.resend = new Resend(apiKey);
+    }
 
+    public CodigoConfirmacaoModel gerarEEnviarCodigoConfirmacao(String email) {
 
-    public CodigoConfirmacaoModel gerarEEnviarCodigoConfirmacao(String email){
         CodigoConfirmacaoModel codigoConfirmacao;
 
         Integer numeroAleatorio = new Random().nextInt(1000, 9999);
         LocalDateTime dataExpiracao = LocalDateTime.now().plusMinutes(10);
 
-        if(codigoConfirmacaoRepository.findByEmail(email).isEmpty()) {
+        if (codigoConfirmacaoRepository.findByEmail(email).isEmpty()) {
+
             codigoConfirmacao = new CodigoConfirmacaoModel(
-                null,
-                numeroAleatorio,
-                email,
-                dataExpiracao
+                    null,
+                    numeroAleatorio,
+                    email,
+                    dataExpiracao
             );
-        }else{
-            codigoConfirmacao = codigoConfirmacaoRepository.findByEmail(email).get();
+
+        } else {
+
+            codigoConfirmacao = codigoConfirmacaoRepository
+                    .findByEmail(email)
+                    .get();
+
             codigoConfirmacao.setCodigoConfirmacao(numeroAleatorio);
             codigoConfirmacao.setDataExpiracao(dataExpiracao);
         }
 
-        enviarEmailComCodigoDoUsuario(email, numeroAleatorio, "Código de confirmação", "cadastro");
+        enviarEmailComCodigoDoUsuario(
+                email,
+                numeroAleatorio,
+                "Código de confirmação",
+                "cadastro"
+        );
 
         return codigoConfirmacaoRepository.save(codigoConfirmacao);
     }
 
-
-    //Métodos privados
-    /*private Boolean enviarEmailComCodigoDoUsuario(String email, Integer codigo, String cabecalho, String acao) {
-        SimpleMailMessage message = new SimpleMailMessage();
-
-        message.setTo(email);
-        message.setSubject(String.format(cabecalho));
-        message.setText("O seu código de " + acao + " é: " + codigo+"\nEle expira em 10 minutos!");
-
-        try {
-            mailSender.send(message);
-            return true;
-        } catch (Exception e) {
-            throw new RequestException("Erro ao enviar o código de confirmação para o email informado!");
-        }*/
     private Boolean enviarEmailComCodigoDoUsuario(
             String email,
             Integer codigo,
             String cabecalho,
             String acao
     ) {
-        SimpleMailMessage message = new SimpleMailMessage();
 
-        message.setTo(email);
-        message.setSubject(cabecalho);
-        message.setText(
-                "O seu código de " + acao + " é: " + codigo +
-                        "\nEle expira em 10 minutos!"
-        );
+        String texto = "O seu código de " + acao + " é: " + codigo
+                + "\nEle expira em 10 minutos!";
+
+        CreateEmailOptions params = CreateEmailOptions.builder()
+                .from("NFT Gestor <onboarding@resend.dev>")
+                .to(email)
+                .subject(cabecalho)
+                .text(texto)
+                .build();
 
         try {
-            mailSender.send(message);
+
+            resend.emails().send(params);
+
             return true;
 
-        } catch (Exception e) {
+        } catch (ResendException e) {
+
             e.printStackTrace();
 
             throw new RequestException(
@@ -92,5 +100,4 @@ public class CodigoConfirmacaoService {
             );
         }
     }
-
 }
